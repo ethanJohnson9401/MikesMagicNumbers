849e9d2a06e50d375578f547efca6384024a344d
ethanJohnson9401 <ethan.johnson9401@gmail.com>
2026-02-26 13:35:22 -0600

    first attempt very slow

--- MagicNums.cpp
+++ MagicNums.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
+
 
 int main(){
+    const long long int MAX_NUM = 5000000000; // 5 billion 
     
+    for (long long int i = 2; i <= MAX_NUM; i++) {
+        std::cout << "Processing number: " << i << std::endl;
+        unsigned long int tempNum = i;
+        while (tempNum > 1){
+            if (tempNum % 2 == 0) {
+                tempNum /= 2;
+            } else {
+                tempNum = (3 * tempNum) + 1;
+            }
+            std::cout << tempNum << std::endl;
+        }
+
+    }
+
 
     return 0;
 }
